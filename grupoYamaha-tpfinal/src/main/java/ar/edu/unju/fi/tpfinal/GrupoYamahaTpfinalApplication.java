9f3e891788da206b000da012d424cc138bff3de3
sebas <sebas@LAPTOP-PALU9LV1>
2021-06-12 21:33:37 -0300

    task : master tp final

---
+++ grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/GrupoYamahaTpfinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoYamahaTpfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoYamahaTpfinalApplication.class, args);
+	}
+
+}
