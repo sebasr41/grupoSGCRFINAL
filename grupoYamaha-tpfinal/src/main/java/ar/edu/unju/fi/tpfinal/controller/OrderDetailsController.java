f9a6bb9234cf732b2ea70f259499b22074edc210
Cristian Michel <cristianmichel33@gmail.com>
2021-06-17 19:59:46 -0300

    Mitad del proyecto andando

--- grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/OrderDetailsController.java
+++ grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/OrderDetailsController.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.fi.tpfinal.controller;
 
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.Random;
 
 import javax.validation.Valid;
 
@@ -106,9 +108,17 @@ public class OrderDetailsController {
 
 			 orderdetails.setOrders(null);
 			 products.ifPresent(orderdetails::setProducts);
+			 orderdetails.setPriceEach(orderdetails.getProducts().getBuyPrice());
+			 
+			 
 			 Long id = orderdetailsService.guardarOrderDetails(orderdetails).getOrderNumber();
 			 orders.setOrderNumber(id);
-
+			 LocalDate hoy = LocalDate.now();
+			 orders.setOrderDate(hoy);
+			 Random r = new Random(); 
+			 
+			 orders.setShippedDate(hoy.plusDays(r.nextInt(10)));
+			 orders.setStatus("Procesando");
 			 orderService.guardarOrders(orders);
 				//modelView.addObject("orders", orderService.obtenerOrders());
 				//modelView.addObject("orderDetails", orderdetailsService.obtenerOrderDetails());
