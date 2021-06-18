1f65b4f3283f6af79ee95515729f40ed13e7e752
3215U <idproinformatica@gmail.com>
2021-06-18 20:49:46 -0300

    asks repo custommers repsonsable benicio

---
+++ grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/repository/ICustomersRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.tpfinal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unju.fi.tpfinal.model.Customers;
+
+public interface ICustomersRepository extends CrudRepository<Customers, Long>{
+
+}
