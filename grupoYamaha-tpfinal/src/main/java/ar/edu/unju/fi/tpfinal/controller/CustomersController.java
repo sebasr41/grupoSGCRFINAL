5e5cd17b7504da7bc81b0e3fd514a8d27f65be89
Cristian Michel <cristianmichel33@gmail.com>
2021-06-19 19:41:42 -0300

    customer funcionando

--- grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/CustomersController.java
+++ grupoYamaha-tpfinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/CustomersController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.tpfinal.model.Customers;
 import ar.edu.unju.fi.tpfinal.model.Employees;
+import ar.edu.unju.fi.tpfinal.model.Offices;
 import ar.edu.unju.fi.tpfinal.model.OrderDetails;
 import ar.edu.unju.fi.tpfinal.model.Orders;
 import ar.edu.unju.fi.tpfinal.model.ProductLines;
@@ -31,15 +32,89 @@ import ar.edu.unju.fi.tpfinal.service.imp.CustomersServiceImp;
 @Controller
 public class CustomersController {
 	
+
+	@Autowired
+	private Orders orders;	
+
+	@Autowired
+	private IOrdersService orderService;
+	
+	@Autowired
+	private ICustomersService customersService;
+
+	@Autowired
+	private CustomersServiceImp customersServiceImp; 
+	
+	@Autowired
+	private OrderDetails orderdetails;
+	
+	@Autowired
+	private Employees employes;
+	
+	@Autowired
+	private IEmployeesService employeesService;
+	
+	
 @Autowired
 private Customers customers;
+
+@Autowired
+private Employees employ;
+
+@Autowired
+private IEmployeesService employservice;
 
 //NO es el verdadero controller lo subo rpovisoriamente solo para probar
 
 @GetMapping("/crearusuario")
 public String getCustomersPage(Model model){
-	model.addAttribute(customers);
+	model.addAttribute("customers",customers);
+	model.addAttribute("employs",  employservice.obtenerEmployees());
 	return "nuevo-usuario";
 }
+
+@PostMapping("/usuario-guardar")
+public ModelAndView getGuardarCustomersPage(@Valid @ModelAttribute("customers")Customers customers, BindingResult resultadoValidacion) {
+	ModelAndView modelView;
+	if(resultadoValidacion.hasErrors()) {
+	modelView= new ModelAndView("nuevo-usuario"); 
+	
+	modelView.addObject("customers",customers);
+	modelView.addObject("employs",  employservice.obtenerEmployees());
+	modelView.addObject("bandera", true);
+	return modelView;
+
+	}
+	
+	else {
+	ModelAndView model = new ModelAndView("redirect:/customer-list");
+	
+	Optional<Employees> employees = employeesService.getEmployeesPorId(customers.getEmployees().getEmployeeNumber());
+	//Optional<Employees> employees = employeesService.getEmployeesPorId(customers.getEmployees().getgetProductLinesName());
+
+	employees.ifPresent(customers::setEmployees);
+
+		
+	customersService.guardarCustomers(customers);
+	
+	
+	
+	return model;
+	}		
+	
+}
+
+@GetMapping("/customer-list")
+public ModelAndView getComprasPage() {
+	ModelAndView model = new ModelAndView("lista-usuario");
+	
+	
+	model.addObject("customers", customersService.obtenerCustomers());
+
+	
+	return model;
+
+}
+
 
 }
